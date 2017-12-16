6d4a5acc82c7964f3ef259e9b485db4649cad680
15891797752 <33649615+15891797752@users.noreply.github.com>
2017-12-16 13:44:45 +0800

    aaa

--- sshs/sshs-core/src/main/resources/scripts/sshs/js/component.js
+++ sshs/sshs-core/src/main/resources/scripts/sshs/js/component.js
@@ -1,128 +1,7 @@
-/**
- * bootstrapTable ajax获取后台数据 paginator插件扩展，以增加通用性 author:Suny date:2017-10-10
- */
-$.fn.extend({
-	getElementsJson : function(parameter) {// 将form组件内的表单元素打包成json对象
-		var params = $(this).serializeArray();
-		if (!parameter) {
-			parameter = {};
-		}
-		for (x in params) {
-			parameter[params[x].name] = params[x].value;
-		}
-		return parameter;
-	},
-	loadData : function(params) {// bootstrapTable 扩展 初始化
-		if (!params) {
-			params = {};
-		} else {
-			params = {
-				"variables" : params
-			};
-		}
-		$(this).bootstrapTable("destroy");
-		var columns;
-		if (typeof $(this).data("columns") === 'string') {
-			columns = JSON.parse($(this).data("columns").replace(/\'/g, "\""));
-		} else {
-			columns = $(this).data("columns");
-		}
-		if (typeof params === 'string') {
-			params = JSON.parse(params);
-		}
-		$(this).bootstrapTable({
-			url : $(this).data("url") ? $(this).data("url") : "", // 请求后台的URL（*）
-			method : $(this).data("method") ? $(this).data("method") : 'post', // 请求方式（*）
-			toolbar : '#toolbar', // 工具按钮用哪个容器
-			striped : $(this).data("striped") ? $(this).data("striped") : false, // 是否显示行间隔色
-			cache : false, // 是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
-			pagination : $(this).data("pagination") ? $(this).data("pagination") : false, // 是否显示分页（*）
-			sortable : true, // 是否启用排序
-			sortName : $(this).data("sortName"), // 初始化的时候排序的字段
-			sortOrder : $(this).data("sortOrder"), // 排序方式
-			queryParams : params, // 传递参数（*）
-			sidePagination : "server", // 分页方式：client客户端分页，server服务端分页（*）
-			contentType : "application/json", // "application/x-www-form-urlencoded",
-												// //请求数据内容格式 默认是
-												// application/json
-												// 自己根据格式自行服务端处理
-			dataType : "json", // 期待返回数据类型
-			pageNumber : 1, // 初始化加载第一页，默认第一页
-			pageSize : $(this).data("pageSize") ? $(this).data("pageSize") : 10, // 每页的记录行数（*）
-			pageList : $(this).data("pageList") ? $(this).data("pageList"):[ 5,10,25,50,100], // 可供选择的每页的行数（*）
-			// search : true, // 是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
-			// strictSearch : true,
-			dataField : "rows",
-			totalField : "totalCount",
-			showColumns : true, // 是否显示所有的列
-			showRefresh : true, // 是否显示刷新按钮
-			minimumCountColumns : 2, // 最少允许的列数
-			clickToSelect : true, // 是否启用点击选中行
-			// height : 500, //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
-			uniqueId : "ID", // 每一行的唯一标识，一般为主键列
-			showToggle : $(this).data("showToggle") ? $(this).data("showToggle") : true, // 是否显示详细视图和列表视图的切换按钮
-			// cardView : false, //是否显示详细视图
-			// detailView : false, //是否显示父子表
-			columns : columns,
-			onPostBody:function(){
-				$(".form-select2").select2({theme:"bootstrap",width: 'resolve'}); 
-				$('.form-switch').bootstrapSwitch();
-			}
-		});
-	},
-	getDataEdited : function(useCurrentPage) {
-		var data1 = $(this).bootstrapTable('getData', useCurrentPage);
-		if(data1 && data1.constructor === Array){
-			var data2 =JSON.stringify(data1);//实现对象复制
-			var data = JSON.parse(data2);
-			var elements = $("[index],[field]");
-			if(elements && elements.length>0){
-				for(var x=0;x<elements.length;x++){
-						var element = elements.get(x);
-						if(element && element.tagName=='INPUT' || element.tagName=="SELECT" ){
-							var value=null;
-							if(element.tagName=='INPUT'  && element.className && element.className.indexOf("form-switch")>=0 ){
-								value = $(element).bootstrapSwitch('state');
-							} else {
-								value = element.value;
-							}
-							if(data[parseInt(element.getAttribute("index"))]){
-								data[parseInt(element.getAttribute("index"))][element.getAttribute("field")] = value;
-							}
-						}
-				}
-			}
-		}
-		return data;
-	}
-});
 
-$.extend({
-	showPage:function(url,param,target){
-			// alert("showpage:"+url);
-			if(target){
-				$(target).load(url,$.extend({"_pageType":"body"}, param));
-			}else{
-				$("body").load(url,$.extend({"_pageType":"body"}, param));
-			}
-		},
-	sendRequest : function(url,data) {
-			$.ajax({
-				type : "post",
-				cache: false,
-				async : false,
-				dataType : "json",
-				contentType : "application/json",
-				url : url,
-				data : JSON.stringify(data),
-				success : function(result) {
-					bootbox.alert(result["code"]+""+result["msg"]);
-				},
-			   error:function(resutl){
-				   bootbox.alert(result["code"]+""+result["msg"]);
-				}
-			});
-		}
+$(document).ready(function() {
+	$("select").select2({theme:"bootstrap",width: 'resolve'}); 
+	$('input[type="checkbox"], input[type="radio"]').filter('[data-switch-init]').bootstrapSwitch();
 });
 
 /**
@@ -216,10 +95,6 @@ Date.prototype.format = function(fmt) { // author: meizz
 	return fmt;
 };
 
-$(document).ready(function() {
-	$("select").select2({theme:"bootstrap",width: 'resolve'}); 
-	$('input[type="checkbox"], input[type="radio"]').filter('[data-switch-init]').bootstrapSwitch();
-});
 /**
  * 日历空间初始化
  */
---
+++ sshs/sshs-core/src/main/resources/scripts/sshs/js/jquery-ext.js
@@ -0,0 +1,134 @@
+/**
+ * jquery扩展，以增加通用性
+ * 
+ * @author:Suny
+ * @date:2017-10-10
+ */
+$.fn.extend({
+	/**
+	 * 将form组件内的表单元素打包成json对象
+	 * 
+	 * @param {parameter}
+	 *            需扩展的json对象
+	 * @method getElementsJson
+	 */
+	getElementsJson : function(parameter) {
+		var params = $(this).serializeJson();
+		if (!parameter) {
+			parameter = {};
+		}
+		return $.extend(parameter, params);
+	},
+	/**
+	 * 将form里面的内容序列化成json 相同的checkbox用分号拼接起来
+	 * 
+	 * @method serializeJson
+	 */
+	serializeJson : function() {
+		var serializeObj = {}, array = this.serializeArray();
+		$(array).each(function() {
+			if (serializeObj[this.name]) {
+				serializeObj[this.name] += ';' + this.value;
+			} else {
+				serializeObj[this.name] = this.value;
+			}
+		});
+		return serializeObj;
+	},
+	/**
+	 * 将json对象赋值给form
+	 * 
+	 * @param {data}
+	 *            需要给form赋值的json对象
+	 * @method putJsonElements
+	 */
+	putJsonElements : function(data) {
+		   var obj = data;
+		    var key,value,tagName,type,arr;
+		    for(x in obj){
+		        key = x;
+		        value = obj[x];
+		        $("[name='"+key+"'],[name='"+key+"[]']").each(function(){
+		            tagName = $(this)[0].tagName;
+		            type = $(this).attr('type');
+		            if(tagName=='INPUT'){
+		                if(type=='radio'){
+		                    $(this).attr('checked',$(this).val()==value);
+		                }else if(type=='checkbox'){
+		                    arr = value.split(',');
+		                    for(var i =0;i<arr.length;i++){
+		                        if($(this).val()==arr[i]){
+		                            $(this).attr('checked',true);
+		                            break;
+		                        }
+		                    }
+		                }else{
+		                    $(this).val(value);
+		                }
+		            }else if(tagName=='SELECT' || tagName=='TEXTAREA'){
+		                $(this).val(value);
+		            }
+		             
+		        });
+		    } 
+	},
+	/**
+	 * 显示指定页面
+	 */
+	showPage : function(url, param) {
+		$(this).load(url, $.extend({
+			"_pageType" : "body"
+		}, param));
+	},
+	/**
+	 * 从后台获取数据，并回填到页面
+	 */
+	loadData : function(url, param) {
+		$.getJSON(url, param, function(data) {
+			$(this).putJsonElements(data);
+		});
+	}
+});
+
+$.extend({
+	/**
+	 * 显示指定页面
+	 */
+	showPage : function(url, param, target) {
+		if (target) {
+			target.showPage(url, param);
+		} else {
+			$("body").showPage(url, param);
+		}
+	},
+	/**
+	 * 从后台获取数据，并回填到页面
+	 */
+	loadData : function(url, param, target) {
+		if (target) {
+			target.loadData(url, param);
+		} else {
+			$("body").loadData(url, param);
+		}
+	},
+	/**
+	 * 发送数据请求到后台，并执行对应服务
+	 */
+	sendRequest : function(url, data) {
+		$.ajax({
+			type : "post",
+			cache : false,
+			async : false,
+			dataType : "json",
+			contentType : "application/json",
+			url : url,
+			data : JSON.stringify(data),
+			success : function(result) {
+				bootbox.alert(result["code"] + "" + result["msg"]);
+			},
+			error : function(resutl) {
+				bootbox.alert(result["code"] + "" + result["msg"]);
+			}
+		});
+	}
+});
