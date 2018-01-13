7bc103f97f16637894bc22c97d9c2e6633e70896
15891797752 <33649615+15891797752@users.noreply.github.com>
2018-01-13 21:29:47 +0800

    aa

--- sshs/sshs-core/src/main/java/com/sshs/core/message/Message.java
+++ sshs/sshs-core/src/main/java/com/sshs/core/message/Message.java
@@ -2,11 +2,14 @@ package com.sshs.core.message;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -19,6 +22,7 @@ import com.sshs.constant.Global;
  *
  */
 public class Message {
+	private static Log logger = LogFactory.getLog(Message.class);
 	String code;
 	String msg;
 
@@ -35,22 +39,28 @@ public class Message {
 	}
 
 	public static String getMessage(String code) {
-		Locale locale = null;
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
-				.getRequest();
-		String local = request.getParameter("locale");
-		if (StringUtils.isNotEmpty(local) && local.contains(Global.CHARACTER_UNDERLINE)) {
-			locale = new Locale(local.split(Global.CHARACTER_UNDERLINE)[0], local.split(Global.CHARACTER_UNDERLINE)[1]);
-		} else {
-			locale = (Locale) request.getSession().getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME);
-		}
-		if (locale == null) {
-			locale = request.getLocale();
-		}
-		ResourceBundle resource = ResourceBundle.getBundle("i18n/messages", locale);
 		try {
+			Locale locale = null;
+			HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+					.getRequest();
+			String local = request.getParameter("locale");
+			if (StringUtils.isNotEmpty(local) && local.contains(Global.CHARACTER_UNDERLINE)) {
+				locale = new Locale(local.split(Global.CHARACTER_UNDERLINE)[0],
+						local.split(Global.CHARACTER_UNDERLINE)[1]);
+			} else {
+				locale = (Locale) request.getSession()
+						.getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME);
+			}
+			if (locale == null) {
+				locale = request.getLocale();
+			}
+			ResourceBundle resource = ResourceBundle.getBundle("i18n/messages", locale);
+
 			String message = new String(resource.getString(code).getBytes("ISO-8859-1"), "UTF-8");
 			return message;
+		} catch (MissingResourceException e) {
+			logger.debug("message:[" + code + "]not found!");
+			return code;
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 			return code;
